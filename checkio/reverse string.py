5bf41936eefd75d87806c7b8c100aa3a5bd69b09
Kevinliao0857 <kevinliao0857@gmail.com>
2021-05-31 14:34:36 -0700

    commit for multiple questions

---
+++ checkio/reverse string.py
@@ -0,0 +1,17 @@
+def backward_string(val: str) -> str:
+    # your code here
+    strlength = len(val)
+    slicedString = val[strlength::-1]
+    return (slicedString)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(backward_string('val'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert backward_string('val') == 'lav'
+    assert backward_string('') == ''
+    assert backward_string('ohho') == 'ohho'
+    assert backward_string('123456789') == '987654321'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
