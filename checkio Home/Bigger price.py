eee1b0142d91250b820d00fd20851fbed4e7c2a8
Kevinliao0857 <kevinliao0857@gmail.com>
2021-06-01 14:45:28 -0700

    Create Bigger price.py

---
+++ checkio Home/Bigger price.py
@@ -0,0 +1,40 @@
+def bigger_price(limit: int, data: list) -> list:
+    """
+        TOP most expensive goods
+    """
+    # your code here
+    sort_list = sorted(data, key=lambda x: x["price"])[-limit:]
+    sort_list.reverse()
+    return sort_list
+
+#shortened method
+
+return sorted(data, key=lambda x: x['price'], reverse=1)[:limit]
+
+if __name__ == '__main__':
+    from pprint import pprint
+    print('Example:')
+    pprint(bigger_price(2, [
+        {"name": "bread", "price": 100},
+        {"name": "wine", "price": 138},
+        {"name": "meat", "price": 15},
+        {"name": "water", "price": 1}
+    ]))
+
+    # These "asserts" using for self-checking and not for auto-testing
+    assert bigger_price(2, [
+        {"name": "bread", "price": 100},
+        {"name": "wine", "price": 138},
+        {"name": "meat", "price": 15},
+        {"name": "water", "price": 1}
+    ]) == [
+        {"name": "wine", "price": 138},
+        {"name": "bread", "price": 100}
+    ], "First"
+
+    assert bigger_price(1, [
+        {"name": "pen", "price": 5},
+        {"name": "whiteboard", "price": 170}
+    ]) == [{"name": "whiteboard", "price": 170}], "Second"
+
+    print('Done! Looks like it is fine. Go and check it')
