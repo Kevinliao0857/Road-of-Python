d480a3544a558032a38226c1ad8eb6ae7a28841f
Kevinliao0857 <kevinliao0857@gmail.com>
2021-06-01 00:50:12 -0700

    Create Backward each word.py

---
+++ checkio Home/Backward each word.py
@@ -0,0 +1,26 @@
+import re
+def backward_string_by_word(text: str) -> str:
+    # your code here
+    newList = []
+    res = re.split(r'(\s+)', text)
+    for word in res:
+        newList.append(word[::-1])
+    return ''.join(newList)
+    
+
+#shorter method
+
+return " ".join([i[::-1] for i in text.split(" ")])
+    
+    
+if __name__ == '__main__':
+    print("Example:")
+    print(backward_string_by_word('hello'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert backward_string_by_word('') == ''
+    assert backward_string_by_word('world') == 'dlrow'
+    assert backward_string_by_word('hello world') == 'olleh dlrow'
+    assert backward_string_by_word('hello   world') == 'olleh   dlrow'
+    assert backward_string_by_word('welcome to a game') == 'emoclew ot a emag'
+    print("Coding complete? Click 'Check' to earn cool rewards!")
