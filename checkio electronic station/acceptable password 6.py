32ec748c07b71a068575b23ffb87659b9d413455
Kevinliao0857 <kevinliao0857@gmail.com>
2021-07-23 23:27:38 -0700

    Create acceptable password 6.py

---
+++ checkio electronic station/acceptable password 6.py
@@ -0,0 +1,74 @@
+# Taken from mission Acceptable Password V
+
+# Taken from mission Acceptable Password IV
+
+# Taken from mission Acceptable Password III
+
+# Taken from mission Acceptable Password II
+
+# Taken from mission Acceptable Password I
+
+def is_acceptable_password(password: str) -> bool:
+
+    condition1 = len(password) > 6
+    condition2 = any(map(str.isdigit, password)) and not password.isdigit()
+    condition3 = len(password) > 9
+    condition4 = 'password' not in password.lower()
+    
+    if 'password' in password.lower()or len(set(password))<3:
+        return False
+    return condition1 and (condition2 or condition3) and condition4
+
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('short54') == True
+    assert is_acceptable_password('muchlonger') == True
+    assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('muchlonger5') == True
+    assert is_acceptable_password('sh5') == False
+    assert is_acceptable_password('1234567') == False
+    assert is_acceptable_password('12345678910') == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password("short") == False
+    assert is_acceptable_password("short54") == True
+    assert is_acceptable_password("muchlonger") == True
+    assert is_acceptable_password("ashort") == False
+    assert is_acceptable_password("muchlonger5") == True
+    assert is_acceptable_password("sh5") == False
+    assert is_acceptable_password("1234567") == False
+    assert is_acceptable_password("12345678910") == True
+    assert is_acceptable_password("password12345") == False
+    assert is_acceptable_password("PASSWORD12345") == False
+    assert is_acceptable_password("pass1234word") == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+if __name__ == "__main__":
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password("short") == False
+    assert is_acceptable_password("short54") == True
+    assert is_acceptable_password("muchlonger") == True
+    assert is_acceptable_password("ashort") == False
+    assert is_acceptable_password("muchlonger5") == True
+    assert is_acceptable_password("sh5") == False
+    assert is_acceptable_password("1234567") == False
+    assert is_acceptable_password("12345678910") == True
+    assert is_acceptable_password("password12345") == False
+    assert is_acceptable_password("PASSWORD12345") == False
+    assert is_acceptable_password("pass1234word") == True
+    assert is_acceptable_password("aaaaaa1") == False
+    assert is_acceptable_password("aaaaaabbbbb") == False
+    assert is_acceptable_password("aaaaaabb1") == True
+    assert is_acceptable_password("abc1") == False
+    assert is_acceptable_password("abbcc12") == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
