8cb8c769edf0118c15d284c1584b44ad51e234e6
Kevinliao0857 <kevinliao0857@gmail.com>
2021-08-19 01:22:03 -0700

    Create testfizzbuzz.rb

---
+++ checkio electronic station/testfizzbuzz.rb
@@ -0,0 +1,15 @@
+num = 12345
+
+if num % 3 == 0 && num % 5 == 0
+    puts "#{num}. FIZZBUZZ!"
+
+elsif num % 3 == 0
+    puts "#{num}. FIZZ!"
+
+elsif num % 5 == 0
+    puts "#{num}. BUZZ!"
+
+else
+    puts "#{num}."
+end
+end
