3c56a7d362f23e6a503dd43127b3c4f0cec69ad1
Kevinliao0857 <kevinliao0857@gmail.com>
2021-08-25 21:04:36 -0700

    Update surjection strings.py

--- checkio electronic station/surjection strings.py
+++ checkio electronic station/surjection strings.py
@@ -1,3 +1,11 @@
+# Maybe it's a cipher? Maybe, but we don’t know for sure.
+
+# Maybe you can call it "homomorphism" ? I wish I knew this word before.
+
+# You need to check that the 2 given strings are isometric. This means that a character from one string can become a match for characters from another string.
+
+# One character from one string can correspond only to one character from another string. Two or more characters of one string can correspond to one character of another string, but not vice versa.
+
 def isometric_strings(a, b):
     return len(set(zip(a, b))) == len(set(a))
 
